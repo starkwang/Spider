881c471cb25972266c6f2c0224cb89b3bb7ad536
Willian Zhang <Willian@Willian-Zhang.com>
2016-05-03 17:28:39 +0800

    feature: source map debugging

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,20 @@ var gulp = require('gulp'),
     clean = require('gulp-clean'),
     concat = require('gulp-concat'),
     babel = require('gulp-babel')
+    ,sourcemaps = require('gulp-sourcemaps')
+
 
 gulp.task('default', ['watch', 'babel']);
 
 gulp.task("babel", function() {
     return gulp
         .src('src/*.js')
+        .pipe(sourcemaps.init())
         .pipe(babel({
-            presets:['es2015']
+            presets:['es2015'],
+            sourceMaps: "both"
         }))
+        .pipe(sourcemaps.write('.', {includeContent: false, sourceRoot: '../src'}))
         .pipe(gulp.dest('build/'));
 });
 
